a9d51899592007c585591dce74beb10e487095b3
MX Development <37406242+ElMuchoLoco@users.noreply.github.com>
2023-03-15 10:55:05 +0000

    Added in New File Path Dependenices & Images / Icons Assets

---
+++ app/paths/imagePath.js
@@ -0,0 +1,5 @@
+export default {
+    libraryIcon : require('../screens/anima-imports/images/libary-1.png'),
+    mapIcon: require('../screens/anima-imports/images/map-icon-1.png'),
+    wanderIcon: require('../screens/anima-imports/images/wander1.png')
+}
